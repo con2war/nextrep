062768aee24786b9cc57bd4ad023fed3071e08b0
con2war <64367519+con2war@users.noreply.github.com>
2025-01-31 17:07:55 +0000

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,6 @@ export const config = {
     '/api/user/:path*',
     '/api/workouts/:path*',
     '/api/generate-workout',
-    '/((?!api/auth/login|api/auth/logout|api/auth/callback|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api/auth/login|api/auth/logout|api/auth/callback|api/auth/profile|_next/static|_next/image|favicon.ico).*)'
   ]
 } 
