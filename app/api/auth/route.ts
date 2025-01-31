c6516466ef4c3dd05d7905acf1b83991baf96185
con2war <64367519+con2war@users.noreply.github.com>
2025-01-31 18:16:25 +0000

    Delete app/api/auth/route.ts

--- app/api/auth/route.ts
+++
@@ -1,9 +0,0 @@
-import { handleAuth } from '@auth0/nextjs-auth0/edge'
-
-export const runtime = 'edge'
-export const dynamic = 'force-dynamic'
-export const fetchCache = 'force-no-store'
-
-const handler = handleAuth()
-export const GET = handler
-export const POST = handler 
